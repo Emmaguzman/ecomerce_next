4bda3c2ce6dbeef7fea92f3f9e5926e562a898dc
Emmanuel Guzman <emguzman@indracompany.com>
2021-10-18 09:21:56 -0300

    Configuracion scss estructura

---
+++ components/Header/Header.js
@@ -0,0 +1,9 @@
+import TopBar from './TopBar/TopBar'
+import Menu from './Menu/Menu'
+
+export default function Header() {
+  return <div className="header">
+      <TopBar/>
+      <Menu/>
+  </div>;
+}
